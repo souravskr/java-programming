8be2acaacd21ca20e97f0a0500df73958ff57052
Sourav <souravsker@gmail.com>
2021-02-23 12:24:55 -0330

    Assignment 1 and 2

--- src/com/coffeebean/Main.java
+++ src/com/coffeebean/Main.java
@@ -1,8 +1,27 @@
 package com.coffeebean;
 
+
+import edu.duke.DirectoryResource;
+import edu.duke.FileResource;
+import edu.duke.Point;
+import edu.duke.Shape;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        DirectoryResource dr = new DirectoryResource();
+        for (File f :
+                dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            Shape s = new Shape(fr);
+            System.out.println(s);
+        }
+
+
     }
 }
